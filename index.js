1c8e67586e7f6d30e426ccfd86d8b6344ced809c
Jonnier <jonnierosjitas@gmail.com>
2022-04-28 15:40:12 -0500

    Slide listo

---
+++ index.js
@@ -0,0 +1,23 @@
+const grande = document.querySelector('.grande')
+const punto = document.querySelectorAll('.punto')
+
+// Cuando click en punto 
+    // saber la posición del punto
+    // Aplicar un transform translateX al grande
+    // QUITAR la clase activo de TODOS los puntos activo
+    // AÑADIR las clase activo al punto que hemos dado click
+punto.forEach(( cadaPunto, i)=>{
+    punto[i].addEventListener('click',()=>{
+
+        let posición = i
+        let operacion = posición * -50
+
+        grande.style.transform = `translateX(${operacion}%)`
+
+        punto.forEach(( cadaPunto, i)=>{
+            punto[i].classList.remove('activo')
+        })
+        punto[i].classList.add('activo')
+        //Hola
+    })
+})
